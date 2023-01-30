40273c84a60109ef4666d9cc1d66dd5ccb212f04
Gabriel Bogado <bogado.gabriel141@gmail.com>
2023-01-30 12:34:49 -0300

    agregandoReactRouter

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <>
+    <BrowserRouter>
+      <Routes>
+        <Route/>
+      </Routes>
+    </BrowserRouter>
+    </>
   );
 }
 
