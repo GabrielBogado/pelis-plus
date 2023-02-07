e0999d33dbf18c530073a02650141052474cd7f0
Gabriel Bogado <bogado.gabriel141@gmail.com>
2023-02-07 17:36:31 -0300

    haciendo-menu

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Header from "./Componentes/Header";
+import Header from "./Componentes/Header/Header.jsx";
 
 function App() {
+  fetch("https://api.fbi.gov/wanted/v1/list")
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .catch((error) => console.log(error));
   return (
     <BrowserRouter>
       <div className="App">
